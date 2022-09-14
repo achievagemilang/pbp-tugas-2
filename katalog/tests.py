13f03cd14f1a97986733c9bb37143102076d809b
achievagemilang <achievafuturagemilang@gmail.com>
2022-09-15 01:09:53 +0700

    add tests

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from katalog.models import CatalogItem
 
 # Create your tests here.
+class KatalogTestCase(TestCase):
+    def setUp(self):
+        CatalogItem.objects.create(item_name = "Bakso", item_price = 5000, item_stock = 20, item_url = "https://www.tokopedia.com/", rating = 5, description = "bakso gaming")
+        CatalogItem.objects.create(item_name = "Oskab", item_price = 15000, item_stock = 1, item_url = "https://www.tokopedia.com/", rating = 10, description = "bakso juga")
+
+    def test_Katalogs_can_speak(self):
+        bakso = CatalogItem.objects.get(item_name="Bakso")
+        oskab = CatalogItem.objects.get(item_name="Oskab")
+
+        self.assertEqual(bakso.item_name, "Bakso")
+        self.assertEqual(oskab.item_price, 15000)
