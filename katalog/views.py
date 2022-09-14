9160d48103b33493d1936482a3bf383e6045e6d3
achievagemilang <achievafuturagemilang@gmail.com>
2022-09-14 20:56:07 +0700

    pre-deployment (tahap 1-3)

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+list_katalog = CatalogItem.objects.all()
+context = {
+    'list_katalog': list_katalog,
+    'name': 'Achieva Futura Gemilang',
+    'student_id': '2106750452'
+}
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
