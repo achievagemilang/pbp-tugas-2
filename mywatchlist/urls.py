bc6a2d21add9aa2d15aa4a37cecd5cdc111d8056
achievagemilang <achievafuturagemilang@gmail.com>
2022-09-21 23:04:29 +0700

    edit/rerun workflow

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -3,7 +3,7 @@ from mywatchlist.views import show_mywatchlist_html, show_mywatchlist_json, show
 
 app_name = "mywatchlist"
 urlpatterns = [
-    path("html/", show_mywatchlist_html, name="show_mywatchlist"),
+    path("html/", show_mywatchlist_html, name="show_mywatchlist_html"),
     path("json/", show_mywatchlist_json, name="show_mywatchlist_json"),
     path("xml/", show_mywatchlist_xml, name="show_mywatchlist_xml"),
 ]
