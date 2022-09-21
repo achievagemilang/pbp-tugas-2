064c29d101a708109af79aaac5d214eeec8da08b
achievagemilang <achievafuturagemilang@gmail.com>
2022-09-21 23:25:51 +0700

    edit tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,19 +1,18 @@
 from django.test import TestCase, Client
-from django.urls import reverse
 
 # Create your tests here.
 class WatchlistTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        
+
     def test_get_html(self):
-        res = self.client.get(reverse('mywatchlist:show_mywatchlist'))
+        res = self.client.get('/mywatchlist/html/')
         self.assertEqual(res.status_code, 200)
 
     def test_get_xml(self):
-        res = self.client.get(reverse('mywatchlist:show_mywatchlist_xml'))
+        res = self.client.get('/mywatchlist/xml/')
         self.assertEqual(res.status_code, 200)
 
     def test_get_json(self):
-        res = self.client.get(reverse('mywatchlist:show_mywatchlist_json'))
+        res = self.client.get('/mywatchlist/json/')
         self.assertEqual(res.status_code, 200)
